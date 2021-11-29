58221998f57d85bff2891a1fd458299baafdc1ae
voodoo <nprice@pricemail.ca>
2021-11-28 20:14:58 -0500

    Update formatting with ESLint

--- src/management_funcs/GraphStoreManager.ts
+++ src/management_funcs/GraphStoreManager.ts
@@ -1,48 +1,48 @@
-import { join } from 'path';
-import { existsSync, PathOrFileDescriptor } from 'fs';
-import { isDir, makeDirectory } from './FileManager';
-import { GraphStore } from '../types/phol_objects/GraphStore';
+import { join } from "path";
+import { existsSync, PathOrFileDescriptor } from "fs";
+import { isDir, makeDirectory } from "./FileManager";
+import { GraphStore } from "../@types/phol_objects/GraphStore";
 
 export function joinPathToStoreRoot(store: GraphStore, fp: string[]): string {
-	const fn = fp.join('/');
+  const fn = fp.join("/");
 
-	return join(store.pholDir.toString(), fn);
+  return join(store.pholDir.toString(), fn);
 }
 
 export function addFileToStore(
-	store: GraphStore,
-	mkdir = false,
-	...fp: string[]
+  store: GraphStore,
+  mkdir = false,
+  ...fp: string[]
 ): PathOrFileDescriptor | false {
-	// get file path of File to Store
-	// Also requests parent folders be created
+  // get file path of File to Store
+  // Also requests parent folders be created
 
-	const fn = fp.slice(0, -1);
+  const fn = fp.slice(0, -1);
 
-	if (addFolderToStore(store, mkdir, ...fn)) {
-		return joinPathToStoreRoot(store, fp);
-	}
-	return false;
+  if (addFolderToStore(store, mkdir, ...fn)) {
+    return joinPathToStoreRoot(store, fp);
+  }
+  return false;
 }
 
 export function addFolderToStore(
-	store: GraphStore,
-	mkdir = false,
-	...fp: string[]
+  store: GraphStore,
+  mkdir = false,
+  ...fp: string[]
 ): PathOrFileDescriptor | false {
-	// Creates path folders
+  // Creates path folders
 
-	const newPath = joinPathToStoreRoot(store, fp);
+  const newPath = joinPathToStoreRoot(store, fp);
 
-	if (existsSync(newPath)) {
-		if (isDir(newPath)) {
-			return newPath;
-		} else {
-			throw `Not a directory ${newPath}`;
-		}
-	}
-	if (mkdir) {
-		makeDirectory(newPath);
-		return newPath;
-	} else return false;
+  if (existsSync(newPath)) {
+    if (isDir(newPath)) {
+      return newPath;
+    } else {
+      throw `Not a directory ${newPath}`;
+    }
+  }
+  if (mkdir) {
+    makeDirectory(newPath);
+    return newPath;
+  } else return false;
 }
--- src/phol_objects/PholObject.ts
+++ src/phol_objects/PholObject.ts
@@ -1,19 +1,21 @@
-import { GraphStore } from '../types/phol_objects/GraphStore';
+import { GraphStore } from "../@types/phol_objects/GraphStore";
 
 export class PholObject {
-	public repo: GraphStore;
+  public repo: GraphStore;
 
-	constructor(repo: GraphStore, data: string) {
-		this.repo = repo;
-		if (data !== null){
-			this.deserialize(data);
-		}
-	}
-	serialize(){
-		// interface
-		throw 'Unimplemented!';
-	}
-	deserialize(data: string){
-		throw 'Unimplemented!';
-	}
+  constructor(repo: GraphStore, data: string) {
+    this.repo = repo;
+    if (data !== null) {
+      this.deserialize(data);
+    }
+  }
+
+  serialize() {
+    // interface
+    throw "Unimplemented!";
+  }
+
+  deserialize(data: string) {
+    throw "Unimplemented!";
+  }
 }
